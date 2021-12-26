300dc4eac4f5bc482283730963348dc1113076dd
Pavel Samarkin <samarkinpa@gmail.com>
2021-12-25 17:04:08 -0800

    Return climate data as json

--- src/server.rs
+++ src/server.rs
@@ -1,23 +1,46 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::net::SocketAddr;
 
 use rocket::{Config, State};
 use rocket::http::Status;
-
-use itertools::join;
+use rocket::serde::{json::Json, Serialize};
 
 use crate::device_data_provider::DeviceDataProvider;
 
-#[get("/climate")]
-async fn get_climate(device_data_provider: &State<DeviceDataProvider>) -> Result<String, Status> {
+#[derive(Serialize)]
+struct ClimateDeviceData {
+    friendly_name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    temperature_in_c: Option<f32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    humidity: Option<f32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    battery: Option<f32>,
+}
+
+#[derive(Serialize)]
+struct StatusData {
+    climate: HashMap<String, ClimateDeviceData>,
+}
+
+#[get("/status")]
+async fn get_status(device_data_provider: &State<DeviceDataProvider>) -> Result<Json<StatusData>, Status> {
     let response = device_data_provider.get_device_data().await.map_err(|err| {
         error!("Error when calling Govee collector ({}): {}", err.code(), err.message());
         Status::ServiceUnavailable
     })?;
-    let ss = response.devices.into_iter()
-        .map(|data| format!("{}: {}ºC", data.friendly_name, data.temperature_in_c.unwrap_or_default()));
-    let s = join(ss, "\n");
-    Ok(s)
+    let climate = response.devices.into_iter()
+        .map(|data| (
+            data.unique_id,
+            ClimateDeviceData {
+                friendly_name: data.friendly_name,
+                temperature_in_c: data.temperature_in_c,
+                humidity: data.humidity,
+                battery: data.battery,
+            }))
+        .collect();
+    Ok(Json(StatusData { climate }))
 }
 
 pub async fn serve(address: SocketAddr) -> Result<(), Box<dyn Error>> {
@@ -28,7 +51,7 @@ pub async fn serve(address: SocketAddr) -> Result<(), Box<dyn Error>> {
     };
     rocket::custom(config)
         .manage(DeviceDataProvider::new().await)
-        .mount("/", routes![get_climate])
+        .mount("/", routes![get_status])
         .launch()
         .await?;
     Ok(())
